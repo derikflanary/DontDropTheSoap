a5f037be233e82d056259f8ee08cb749cf0d20de
Derik Flanary <derikflanary@gmail.com>
2015-11-12 11:02:08 -0700

    Initial Commit

---
+++ DontDropTheSoap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DontDropTheSoap
+//
+//  Created by Derik Flanary on 11/12/15.
+//  Copyright © 2015 Derik Flanary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DontDropTheSoapTests/DontDropTheSoapTests.m
@@ -0,0 +1,39 @@
+//
+//  DontDropTheSoapTests.m
+//  DontDropTheSoapTests
+//
+//  Created by Derik Flanary on 11/12/15.
+//  Copyright © 2015 Derik Flanary. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DontDropTheSoapTests : XCTestCase
+
+@end
+
+@implementation DontDropTheSoapTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
