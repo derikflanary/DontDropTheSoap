cedc8fd12cd91f360a6fb5332f1f9d73d85f793c
Derik Flanary <derikflanary@gmail.com>
2015-12-18 16:05:07 -0700

    Random x force added to soap every 5 seconds

--- DontDropTheSoap/GameScene.m
+++ DontDropTheSoap/GameScene.m
@@ -19,7 +19,7 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
 @property (nonatomic, strong) SKSpriteNode *soap;
 @property (nonatomic, strong) SKSpriteNode *dish;
 @property (nonatomic, strong) CMMotionManager *motionManager;
-@property (nonatomic, assign) CGFloat xAcceleration;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
 
 @end
 
@@ -27,7 +27,7 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
 
 - (id)initWithSize:(CGSize)size{
     if (self = [super initWithSize:size]) {
-        self.backgroundColor = [SKColor blueColor];
+        self.backgroundColor = [SKColor cyanColor];
         
         _motionManager = [[CMMotionManager alloc] init];
         _motionManager.accelerometerUpdateInterval = 0.2;
@@ -38,7 +38,7 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
 
 - (void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    self.soap = [[SKSpriteNode alloc]initWithColor:[UIColor whiteColor] size:CGSizeMake(100, 30)];
+    self.soap = [[SKSpriteNode alloc]initWithColor:[UIColor whiteColor] size:CGSizeMake(50, 15)];
     self.soap.position = CGPointMake(self.view.frame.size.width/2, 100);
     self.soap.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.soap.size];
     self.soap.physicsBody.dynamic = YES;
@@ -47,9 +47,10 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
     self.soap.physicsBody.categoryBitMask = CollisionCategorySoap;
     self.soap.physicsBody.contactTestBitMask = CollisionCategoryDish;
     self.soap.physicsBody.mass = 0.02;
+    self.soap.physicsBody.friction = 0.0;
     [self addChild:self.soap];
     
-    self.dish = [[SKSpriteNode alloc]initWithColor:[UIColor lightGrayColor] size:CGSizeMake(200, 10)];
+    self.dish = [[SKSpriteNode alloc]initWithColor:[UIColor lightGrayColor] size:CGSizeMake(120, 10)];
     self.dish.position = CGPointMake(self.view.frame.size.width/2, CGRectGetMinY(self.soap.frame));
     self.dish.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.dish.size];
     self.dish.physicsBody.dynamic = NO;
@@ -89,6 +90,26 @@ typedef NS_OPTIONS(uint32_t, CollisionCategory) {
 - (void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
     [self updateSoapPositionFromMotionManager];
+    
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    
+    if (timeSinceLast > 5.0) {
+        [self applyRandomForceToSoap];
+    }
+}
+
+- (void)applyRandomForceToSoap{
+    srand48(time(0));
+    double r = drand48();
+    r = r/5;
+    NSUInteger i = arc4random_uniform(2);
+    
+    if (i == 1) {
+        r = r * -1;
+    }
+    
+    [self.soap.physicsBody applyForce:CGVectorMake(60.0 * r, 0.0)];
+
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
